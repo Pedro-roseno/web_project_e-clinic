d3aac625163897b7673341d253b96c5ca5fff090
RaquelS1lv <raquelsteslva@gmail.com>
2024-12-10 17:00:53 -0300

    Adicionando tela de chatbot

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import FormProduto from './views/chatbot/TelaLogin';
+
+
+function App() {
+
+  return (
+ 
+    <div className="App">
+     
+      
+      <FormProduto/>
+
+ 
+    </div>
+  );
+ }
+export default App;
+
