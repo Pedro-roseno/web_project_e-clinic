42e3383d9b4262d79d7b0105a46e036de5c7c2af
MatheusforAll <matheusrafael1007@gmail.com>
2025-01-04 19:17:45 -0300

    loginversao1

---
+++ src/pages/Tela_chatbot/FormLogin.jsx
@@ -0,0 +1,28 @@
+import "../../styles/global.css";
+import React from "react";
+import { Container, Menu, Input, Button, Icon } from "semantic-ui-react";
+import "./Login.css";
+
+
+const MenuLayout = () => (
+
+  <div id="principal">
+  
+  <div id="navbar_area2">
+          <section id="navbar_section2">
+            <nav id="nav2">
+              <h1>eClinic+</h1>
+              <ul id="ul-area">
+          <li><a href="#inicio">Início</a></li>
+          <li><a href="#sobre">Sobre</a></li>
+          <li><a href="#servicos">Serviços</a></li>
+          <li><a href="#depoimentos">Depoimentos</a></li>
+        </ul> 
+            </nav>
+          </section>
+        </div>
+        </div>
+ 
+    
+);
+export default MenuLayout;
