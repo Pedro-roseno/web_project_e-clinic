937c3b1b04ea0ff8bb49c0bd80164135b684d558
MatheusforAll <matheusrafael1007@gmail.com>
2025-01-05 15:31:49 -0300

    loginversaofinal

--- src/pages/Tela_chatbot/FormLogin.jsx
+++ src/pages/Tela_chatbot/FormLogin.jsx
@@ -1,5 +1,6 @@
 import "../../styles/global.css";
 import React from "react";
+import robo from "../../assets/robo.png";
 import { Container, Menu, Input, Button, Icon } from "semantic-ui-react";
 import "./Login.css";
 
@@ -22,6 +23,9 @@ const MenuLayout = () => (
           </section>
         </div>
         <div id="content_section_rigth">
+          <div id="robo-area">
+         <img src={robo} alt="Logo eClinic+" id="robo"/>
+          </div>
         <div id="password_advice_box"> 
         <p id="p_area">Bem-vindo ao eClinic+</p>
         <p id="p_area2">Estamos felizes por você escolher nossa plataforma para cuidar da sua saúde. Para começar, você pode selecionar uma das opções abaixo:</p>
@@ -34,8 +38,14 @@ const MenuLayout = () => (
         <p id="p_area2">Assim que você escolher uma opção, podemos dar continuidade à sua jornada conosco. Qual opção você gostaria de seguir?</p>
        <p id="p_area2">Equipe eClinic+</p>
        </ul>
-        </div>
+
+        </div>s
       </div>
+
+      <div id="area-baixo" >
+  <input type="text" name="" id="" placeholder="Menssagem" />
+<button id="botao">Enviar</button>
+        </div>
     </div>
         
  
