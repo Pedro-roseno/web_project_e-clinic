ec7d3e5fb06cc7e4a939c5ddf9b10c52e09188c5
MatheusforAll <matheusrafael1007@gmail.com>
2025-01-05 12:23:29 -0300

    loginversao4

--- src/pages/Tela_chatbot/FormLogin.jsx
+++ src/pages/Tela_chatbot/FormLogin.jsx
@@ -21,7 +21,23 @@ const MenuLayout = () => (
             </nav>
           </section>
         </div>
+        <div id="content_section_rigth">
+        <div id="password_advice_box"> 
+        <p id="p_area">Bem-vindo ao eClinic+</p>
+        <p id="p_area2">Estamos felizes por você escolher nossa plataforma para cuidar da sua saúde. Para começar, você pode selecionar uma das opções abaixo:</p>
+       <ul id="ul-area2">
+        <li>1. Cadastrar-se</li>
+        <li>2. Fazer login</li>
+        <li>3. Login de Administrador</li>
+        <li>4. Falar com o Suporte</li>
+        <li>5. Recuperar senha</li>
+        <p id="p_area2">Assim que você escolher uma opção, podemos dar continuidade à sua jornada conosco. Qual opção você gostaria de seguir?</p>
+       <p id="p_area2">Equipe eClinic+</p>
+       </ul>
         </div>
+      </div>
+    </div>
+        
  
     
 );
