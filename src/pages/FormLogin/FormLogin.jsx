41453754b77d662815cdb309db89f6dcc64b69d7
Wendel <wendel.efrain11@gmail.com>
2025-02-06 08:24:32 -0300

    integração de cadastro/login

--- src/pages/FormLogin/FormLogin.jsx
+++ src/pages/FormLogin/FormLogin.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./FormLogin.css";
+import pacienteService from "../../services/Paciente.service.ts";
+import AuthPacienteService from "../../services/AuthPaciente.service.ts";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faLock, faArrowLeft, faHome } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom"; 
+import { faLock, faIdCard, faHome } from "@fortawesome/free-solid-svg-icons";
+import { Link, useLocation } from "react-router-dom";
 
 export const FormLogin = () => {
+  const { state } = useLocation();
+  const [idPaciente, setIdPaciente] = useState();
+
+  useEffect(() => {
+    if (state != null && state.id != null) {
+      pacienteService.getById(state.id).then((response) => {
+        setIdPaciente(response.data.id);
+        setCpf(response.data.cpf);
+        setSenha(response.data.senha);
+      });
+    }
+  }, [state]);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const Login = (event) => {
+    
+      event.preventDefault();
+
+      let authData = {
+        username,
+        password,
+      };
+
+      try {
+        const response = AuthPacienteService.login(authData);
+        console.log("Usuário autenticado com sucesso!", response);
+        // Aqui você pode redirecionar o usuário ou atualizar o estado
+      } catch (err) {
+        setError("Falha ao autenticar: " + err.message);
+      }
+    
+  };
+
   return (
     <div className="login-container">
       <nav className="login-navbar">
-        <Link to="/" className="nav-icon">Home <FontAwesomeIcon icon={faHome} /></Link>
+        <Link to="/" className="nav-icon">
+          Home <FontAwesomeIcon icon={faHome} />
+        </Link>
       </nav>
 
       <div className="login-box">
@@ -16,20 +56,34 @@ export const FormLogin = () => {
           <h2>Login</h2>
           <form className="form-grid">
             <div className="input-group">
-              <FontAwesomeIcon icon={faEnvelope} />
-              <input type="email" placeholder="Email" required />
+              <FontAwesomeIcon icon={faIdCard} />
+              <input
+                value={username}
+                type="text"
+                placeholder="CPF"
+                required
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="input-group">
               <FontAwesomeIcon icon={faLock} />
-              <input type="password" placeholder="Senha" required />
+              <input
+                value={password}
+                type="password"
+                placeholder="Senha"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            <button type="submit">Entrar</button>
+            <button onClick={(event) => Login(event)} type="submit">
+              Entrar
+            </button>
           </form>
-          <Link to="/formCadastro" className="toggle-btn">Criar conta</Link>
+          <Link to="/formCadastro" className="toggle-btn">
+            Criar conta
+          </Link>
         </div>
       </div>
     </div>
   );
 };
-
-
