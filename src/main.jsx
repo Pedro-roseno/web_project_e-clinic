a378e03e164d22be7f3d83c3b3b2cdfe208ed0ec
Wendel <wendel.efrain11@gmail.com>
2025-02-07 15:15:13 -0300

    mensagem de login/cadastro

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Root } from "../src/root";
+import { ToastContainer } from "react-toastify";
+import "react-toastify"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ToastContainer />
     <Root />
   </StrictMode>
 );
